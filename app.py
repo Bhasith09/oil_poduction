3f64932e3e39fb841d3cd2705534e5f117696631
Bhasith <bhasith009@gmail.com>
2025-08-06 10:51:47 +0530

    Prepare for Render deployment with port 0.0.0.0

--- app.py
+++ app.py
@@ -88,5 +88,7 @@ def clear():
         os.remove(CSV_FILE)
     return redirect(url_for('home'))
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
